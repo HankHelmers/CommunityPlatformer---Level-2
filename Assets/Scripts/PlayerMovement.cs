6eac41fde3cf06fefa3ea626a0c48e1ca8ca21f8
HankHelmers <89431423+HankHelmers@users.noreply.github.com>
2021-12-30 20:15:49 -0500

    Level 1 - Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //[SerializeField] private float speed; 
+    // Part 1 - Movement
+    public float speed;
+    private Rigidbody2D body;
+
+    // Part 3 - Animation
+    private Animator anim;
+    private bool isGrounded;
+    private bool isFalling;
+
+    private void Awake()
+    {
+        // Part 1 - Movement
+        body = GetComponent<Rigidbody2D>();
+
+        // Part 3 - Animation
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // PART 1 - Movement ** 
+        float x = Input.GetAxis("Horizontal") * speed; // value defined by Unity, left or A key becomes -1 or 1, 0 when nothing presse
+                                                       // x, y, z
+        body.velocity = new Vector2(x, body.velocity.y);
+
+        // **
+
+        // PART 2 - Jump **
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            // moved in part 3 (animation) body.velocity = new Vector2(body.velocity.x, speed);
+            Jump();
+
+        }
+
+
+        // Flipping 
+        if (x > 0.01f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (x < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // ** 
+
+        // Part 3 - Animation
+        anim.SetBool("isRunning", x != 0);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isFalling", isFalling);
+
+        // Falling 
+        if (body.velocity.y < 0)
+        {
+            isFalling = true; 
+        }
+
+        // ** 
+    }
+
+
+    // PART 3 - ANIMATION
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        isGrounded = false;
+        isFalling = false; 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Animation Controls for Jump
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            isFalling = false; 
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CommunityPlatformerExample
+Unity platformer game for creating tutorial course and introduction to Unity and C# programming. 
